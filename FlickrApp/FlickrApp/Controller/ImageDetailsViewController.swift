3d22c0009ffaa0254efd7554003dd94eed08c062
Gabor Horvath <gab.horv@gmail.com>
2019-10-02 08:44:26 +0200

    Add: New master-detail Xcode project

---
+++ FlickrApp/FlickrApp/Controller/ImageDetailsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  FlickrApp
+//
+//  Created by Gabor Horvath on 2019. 10. 01..
+//  Copyright © 2019. Gabor Horvath. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
